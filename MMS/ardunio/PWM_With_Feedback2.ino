5c9c5d835cf923724eab927eac44a46f118567f6
mustafaMertSarikaya16 <56087346+mustafaMertSarikaya16@users.noreply.github.com>
2021-01-12 21:54:30 +0200

    secound signal added

--- MMS/ardunio/PWM_With_Feedback2.ino
+++ MMS/ardunio/PWM_With_Feedback2.ino
@@ -1,10 +1,13 @@
 /*Bu code 330V-140V arası voltajı 24Vta çevirmek için tasarlanmış Buckconverterın 
 PID control ile 2A civarı output current vermesini sağlamak için
 Duty cycle controlü yapacaktır*/
-#include <PWM.h>
+#include <PWM2.h>
+#define NOP __asm__ __volatile__ ("nop\n\t")
 #define PIN_INPUT 14              // anaog input pin from current sence resistor
-#define PIN_OUTPUT 3             // PWM output pin for duty cycle
-
+#define PIN_OUTPUT1 3             // PWM output pin for duty cycle
+#define PIN_OUTPUT2 10
+#define PIN_OUTPUT3 9             // PWM output pin for duty cycle
+#define PIN_OUTPUT4 11
 int32_t frequency = 50000;  //frequency (in Hz)
 
 /* duty cyle will change with PID feedback but initially it has smallest Value
@@ -32,7 +35,8 @@ Serial.begin(9600);
 
   //PWM setup
   InitTimersSafe(); //set timers
-  bool success = SetPinFrequencySafe(PIN_OUTPUT,frequency);  // set output PWM frequency
+  bool success = SetPinFrequencySafe(PIN_OUTPUT1,frequency);  // set output PWM frequency
+  bool successs = SetPinFrequencySafe(PIN_OUTPUT2,frequency);  // set output PWM frequency
 }
 
 /* This loop countuniously get current sence resistors voltage and make PID calculation 
@@ -41,25 +45,17 @@ Serial.begin(9600);
 
 void loop() {
    PID_Input_Value = analogRead(PIN_INPUT);
-   error = Setpoint - (double)PID_Input_Value;
-   integrated_error = integrated_error + Ki * error;
-   proportional_error = Kp * error;
-   PID_Output_Value = proportional_error + integrated_error;
-   Duty_With_Feedback = Duty + PID_Output_Value; 
+   error = Setpoint - (double)PID_Input_Value;        //Find error with finding differences between set point and current sence input
+   integrated_error = integrated_error + Ki * error;  //keep integrated error value
+   proportional_error = Kp * error;                   //keep proportional error value
+   PID_Output_Value = (proportional_error + integrated_error)*36; //when kp and ki are one it increase duty %5 at maximum error so multiply 36 with PI values
+   Duty_With_Feedback = Duty + PID_Output_Value;      //Duty Values that given in 50Khz prequency
    if(Duty_With_Feedback > 65535.0){Duty_With_Feedback = 65535.0;}
    if(Duty_With_Feedback < 0){Duty_With_Feedback = 1;}
-   Serial.println(Duty_With_Feedback/65535.0);    
-   pwmWriteHR(PIN_OUTPUT, (uint16_t)Duty_With_Feedback);
-delay(1);
 
-   /*
-   Serial.println("-------------------------------- ");
-   Serial.println("PID_Input_Value ");
-   Serial.println(PID_Input_Value);
-   Serial.println("error ");
-   Serial.println(error);
-   Serial.println("Duty_With_Feedback ");
-   Serial.println(Duty_With_Feedback );
-   delay(100);
-   */
+   //Serial.println(Duty_With_Feedback/65535.0);    
+   pwmWriteHR(PIN_OUTPUT1, (uint16_t)Duty_With_Feedback);
+   delayMicroseconds(10);
+   pwmWriteHR(PIN_OUTPUT2, (uint16_t)(65535-Duty_With_Feedback));
+delay(1);
 }
